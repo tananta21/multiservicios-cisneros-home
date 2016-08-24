0cf0fe834ffd059d480eef3624b144b43efbef40
tananta21 <terre.21az@gmail.com>
2016-08-24 01:38:09 -0500

    commit inicial

---
+++ js/home.js
@@ -0,0 +1,16 @@
+
+$(function() {
+
+    var btn_movil = $('#nav-mobile'),
+        menu = $('#menu').find('ul');
+
+    // Al dar click agregar/quitar clases que permiten el despliegue del menú
+    btn_movil.on('click', function (e) {
+        e.preventDefault();
+
+        var el = $(this);
+
+        el.toggleClass('nav-active');
+        menu.toggleClass('open-menu');
+    })
+});
